72510f6133c561041107939753b36e1e5e0404ef
Eleuthur <105822177+Eleuthur@users.noreply.github.com>
2024-02-06 12:22:48 +0000

    Initial game playing function added

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ function getComputerChoice() {
     return choices[number];
 }
 
-console.log(getComputerChoice());
+function playround(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return("It's a tie!!");
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return("You lose! Paper beats Rock");
+        } else {
+            return ("You win! Rock beats Scissors");
+        }
+        }
+    }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
